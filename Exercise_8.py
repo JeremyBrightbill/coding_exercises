985d6a39b296702983674961b327ee3ef66afa91
Jeremy Brightbill <jbrightbill@lasallenetwork.com>
2020-02-03 09:07:34 -0600

    Started Exercise 8

---
+++ Exercise_8.py
@@ -0,0 +1,30 @@
+"""Program to evenly divide pizzas. Prompts for number of people, number of 
+pizzas, and number of slices per pizza. Ensures that the number of slices per 
+person comes out even. Displays the number of pieces each person should get, 
+plus any remainder.
+
+Challenge: Allow only numeric input. Handle pluralization (piece vs. pieces).
+
+WORK IN PROGRESS"""
+
+class Event():
+
+    def __init__(self, people: int, pizzas: int, pieces: int): 
+        self.people = people
+        self.pizzas = pizzas
+        self.pieces = pieces
+
+    def calculate(self): 
+        self.per_person = (self.pizzas * self.pieces) // self.people
+        self.remainder = (self.pizzas * self.pieces) % self.people
+    
+    def __repr__(self): 
+        return \
+f"""{self.people} people with {self.pizzas} pizzas, {self.pieces} slices per pizza. 
+Each person gets {self.per_person} slices of pizza. 
+There are {self.remainder} slices left over."""
+
+if __name__ == "__main__":
+    event = Event(3, 4, 13)
+    event.calculate()
+    print(event)
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Solutions to problems from *Exercises for Programmers: 57 Challenges to Develop Your Coding Skills*, by Brian P. Hogan. Pragmatic Bookshelf, 2015. Amazon: https://www.amazon.com/Exercises-Programmers-Challenges-Develop-Coding-ebook/dp/B0170Q50LK
 
-The exercises are language-agnostic; I used Python 3.8. 
+The exercises are language-agnostic; I used Python 3.8. Since the exercises are quite simple, I have tried to make the solutions as efficient and Pythonic as possible. 
 
 Comments and suggestions welcome! 
