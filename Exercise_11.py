1b38182d70027e3bb21b2b34bbdc1d4ba2b6f80f
Jeremy Brightbill <jbrightbill@lasallenetwork.com>
2020-02-07 16:36:16 -0600

    Working on Exercise 11

--- Exercise_11.py
+++ Exercise_11.py
@@ -1,14 +1,16 @@
-"""Program to convert any currency to US dollars. IN PROGRESS
+"""Program to convert any currency to US dollars. WORK IN PROGRESS
 
 Constraints: Ensure that fractions of a cent are rounded up to
 the next penny. 
 
+My innovation: Use command line arguments instead of prompts
+
 Challenges: 
 * Build a dictionary of conversion rates and prompt 
 for the countries instead of the rates. 
-* Get exchange rates from an API
-* My addition: Use command line arguments instead of prompts."""
+* Get exchange rates from an API."""
 
+import json
 import os
 import requests
 import sys
@@ -20,17 +22,32 @@ API_BASE: str = 'https://openexchangerates.org/api/'
 APP_ID: str = os.environ['APP_ID']
 endpoint: str = os.path.join(API_BASE, ('latest.json?app_id=' + APP_ID))
 
-# TO DO: Wrap this in try except
-response = requests.get(endpoint)
-
 try: 
-    currency: str = sys.argv[1]
-    amount:str = sys.argv[2]
-    output = (currency, amount)
+    input_currency: str = sys.argv[1]
+    input_amount: float = float(sys.argv[2])
 except(IndexError):
     output = "Please enter the currency and the amount"
 
+# TO DO: Make a class
+
+def get_rates(endpoint: str, currency: str) -> float:
+    response = requests.get(endpoint)
+    parsed = response.json() 
+    return parsed['rates'][currency]
+
+# TO DO: Make sure the math is right (understand what the API is returning)
+
+def convert_amount(rate: float, amount: float) -> float:
+    return rate * amount
+
+# TO DO: Wrap this in try except
+
 if __name__ == '__main__':
-    # Temporary test
-    print(response.json())
-    # TO DO: Set up the rest of functionality
+    rate = get_rates(endpoint, input_currency)
+    out_amount = convert_amount(rate, input_amount)
+
+    output = f'{str(input_amount)} units of currency {input_currency} = \
+{str(out_amount)} USD'
+
+    print(output)
+    
